abc85547be36288250c69f366304de941b9a7e16
Lucas César <lucascesarmengo@gmail.com>
2026-01-19 16:55:00 -0300

    Dictionary Exercice solved!

---
+++ DictionaryExercice/Program.cs
@@ -0,0 +1,31 @@
+﻿Dictionary<string, int> votes = new Dictionary<string, int>();
+string path = @"C:\temp\dictionary.txt";
+
+try
+{
+    using (StreamReader sr = File.OpenText(path)) {
+        while (!sr.EndOfStream) {
+            string[] lines = sr.ReadLine().Split(',');
+            string name = lines[0];
+            int vote = int.Parse(lines[1]);
+
+            if (votes.ContainsKey(name))
+            {
+                votes[name] += vote;
+            }
+            else
+            {
+                votes[name] = vote;
+            }
+        }
+    }
+
+    foreach (KeyValuePair<string, int> vote in votes)
+    {
+        Console.WriteLine(vote.Key + ": " + vote.Value);
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("Error: " + e.Message);
+}
