13f2cd5bfcf26f8d8283bbc36d791dc9f967e9e4
Lucas César <lucascesarmengo@gmail.com>
2026-01-19 13:59:56 -0300

    Set Exercice solved by Dr. Nelio Alves

---
+++ SetsExerciceSolved/Entities/LogRecord.cs
@@ -0,0 +1,23 @@
+﻿namespace SetsExerciceSolved.Entities;
+
+public class LogRecord
+{
+    public string Username { get; set; }
+    public DateTime Instant { get; set; }
+
+    public override int GetHashCode()
+    {
+        return Username.GetHashCode();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (!(obj is LogRecord ))
+        {
+            return false;
+        }
+        
+        LogRecord other = (LogRecord)obj;
+        return Username.Equals(other.Username);
+    }
+}
---
+++ SetsExerciceSolved/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using SetsExerciceSolved.Entities;
+
+HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+try {
+    using (StreamReader sr = File.OpenText(path)) {
+        while (!sr.EndOfStream) {
+            string[] line = sr.ReadLine().Split(' ');
+            string name = line[0];
+            DateTime date = DateTime.Parse(line[1]);
+            set.Add(new LogRecord{Username = name, Instant = date});
+            Console.WriteLine(line);
+        }
+
+        Console.WriteLine("Total users: " + set.Count);
+    }
+}
+catch (IOException e) {
+    Console.WriteLine(e.Message);
+}
